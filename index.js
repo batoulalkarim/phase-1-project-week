f0baf5fe43ecb13f90db27d08a17e9b68da2882d
Batoul Alkarim <batoulalkarim1@gmail.com>
2022-02-16 11:13:28 -0500

    created first fetch

--- index.js
+++ index.js
@@ -11,7 +11,37 @@ reviewForm.addEventListener('submit', e => {
     reviewForm.reset();
 })
 
-fetch
+let meals= [];
 
+fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=b')
+.then(res => res.json())
+.then(meals1 => {
+    addToNavBar(meals1.meals[0])
+})
+
+
+
+function addToNavBar(meal){
+ let mealMenu = document.querySelector('#meals-menu')
+ let imgOfMeal = document.createElement('img');
+ imgOfMeal.id = 'meal-menu'
+ imgOfMeal.src = meal.strMealThumb;
+ mealMenu.appendChild(imgOfMeal)
+ imgOfMeal.addEventListener('click', e => {
+     showMealDetails(meal)
+ })
+}
+
+function showMealDetails(meal){
+    let recipe = document.querySelector('#recipe');
+    let mainImg = document.querySelector('#selectedPic')
+    let mealName = document.querySelector('.name');
+    let youtubeLink = document.querySelector('#youtube');
+
+    recipe.textContent = meal.strInstructions;
+    mainImg.src = meal.strMealThumb;
+    mealName.textContent = meal.strMeal;
+    youtubeLink.textContent = meal.strYoutube;
+}
 
 
